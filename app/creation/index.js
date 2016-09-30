41ef4f6f9c826682c4dce61d3ae24f45538d745a
Brave Chai <bravetsia@gmail.com>
2016-09-30 17:10:00 +0800

    add ListView

--- app/creation/index.js
+++ app/creation/index.js
@@ -4,15 +4,31 @@ var React = require('react-native')
 var Icon = require('react-native-vector-icons/Ionicons')
 var Text = React.Text
 var View = React.View
+var ListView = React.ListView
 var StyleSheet = React.StyleSheet
 
 var List = React.createClass({
+  getInitialState: function() {
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    return {
+      dataSource: ds.cloneWithRows([]),
+    };
+  },
+
+  renderRow: function(row) {
+
+  },
+
   render: function() {
     return (
       <View style={styles.container}>
         <View style={styles.header}>
           <Text style={styles.headerTitle}>列表页面</Text>
         </View>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow} 
+        />
       </View>
     )
   }
