52c53d7b8fed50d5f8e041b64ba454f2dcb8a0c7
Brave Chai <bravetsia@gmail.com>
2016-12-12 17:19:44 +0800

    create account upload avatar component

--- app/account/index.js
+++ app/account/index.js
@@ -2,15 +2,74 @@
 
 var React = require('react-native')
 var Icon = require('react-native-vector-icons/Ionicons')
+
 var Text = React.Text
 var View = React.View
 var StyleSheet = React.StyleSheet
+var Dimensions = React.Dimensions
+var width = Dimensions.get('window').width
+var AsyncStorage = React.AsyncStorage
+var TouchableOpacity = React.TouchableOpacity
+var Image = React.Image
 
 var Account = React.createClass({
-  render: function() {
+  getInitialState() {
+    var user = this.props.user || {}
+
+    return {
+      user: user
+    }
+  },
+
+  componentDidMount() {
+    var that = this
+
+    AsyncStorage.getItem('user')
+      .then((data) => {
+        var user
+
+        if (data) {
+          user = JSON.parse(data)
+        }
+
+        if (user && user.accessToken) {
+          that.setState({
+            user: user
+          })
+        }
+      })
+  },
+
+  render() {
+    var user = this.state.user
+
     return (
       <View style={styles.container}>
-        <Text>Account Page</Text>
+        <View style={styles.toolbar}>
+          <Text style={styles.toolbarTitle}>我的账户</Text>
+        </View>
+
+        {
+          !user.avatar
+          ? <TouchableOpacity style={styles.avatarContainer}>
+            <Image source={{uri: user.avatar}} style={styles.avatarContainer}>
+              <View style={styles.avatarBox}>
+                <Image
+                  source={{uri: user.avatar}}
+                  style={styles.avatar} />
+              </View>
+              <Text style={styles.avatarTip}>戳这里换头像</Text>
+            </Image>
+          </TouchableOpacity>
+          : <View style={styles.avatarContainer}>
+            <Text style={styles.avatarTip}>添加狗狗头像</Text>
+            <TouchableOpacity style={styles.avatarBox}>
+              <Icon
+                name='ios-cloud-upload-outline'
+                style={styles.plusIcon} />
+            </TouchableOpacity>
+          </View>
+        }
       </View>
     )
   }
@@ -19,8 +78,62 @@ var Account = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
   },
+
+  toolbar: {
+    flexDirection: 'row',
+    paddingTop: 25,
+    paddingBottom: 12,
+    backgroundColor: '#ee735c'
+  },
+
+  toolbarTitle: {
+    flex: 1,
+    fontSize: 16,
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: '600'
+  },
+
+  avatarContainer: {
+    width: width,
+    height: 140,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#666'
+  },
+
+  avatarTip: {
+    color: '#fff',
+    backgroundColor: 'transparent',
+    fontSize: 14
+  },
+
+  avatarBox: {
+    marginTop: 15,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  avatar: {
+    marginBottom: 15,
+    width: width * 0.2,
+    height: width * 0.2,
+    resizeMode: 'cover',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: width * 0.1
+  },
+
+  plusIcon: {
+    padding: 20,
+    paddingLeft: 25,
+    paddingRight: 25,
+    color: '#999',
+    fontSize: 24,
+    backgroundColor: '#fff',
+    borderRadius: 8
+  }
 });
 
 module.exports = Account;
