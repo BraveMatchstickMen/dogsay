5c03eb2b7da9d26abb2f62cc2f7689cda128ebf4
Brave Chai <brave.chai@blackboard.com>
2017-07-14 19:47:15 +0800

    display record image when there is no video

--- app/edit/index.js
+++ app/edit/index.js
@@ -4,13 +4,45 @@ var React = require('react-native')
 var Icon = require('react-native-vector-icons/Ionicons')
 var Text = React.Text
 var View = React.View
+var Image = React.Image
+var TouchableOpacity = React.TouchableOpacity
 var StyleSheet = React.StyleSheet
+var Dimensions = React.Dimensions
+
+var width = Dimensions.get('window').width
+var height = Dimensions.get('window').height
 
 var Edit = React.createClass({
+  getInitialState() {
+    var user = this.props.user || {}
+
+    return {
+      previewVideo: null
+    }
+  },
+
   render: function() {
     return (
       <View style={styles.container}>
-        <Text>Edit Page</Text>
+        <View style={styles.toolbar}>
+          <Text style={styles.toolbarTitle}>{this.state.previewVideo ? '点击按钮配音' : '理解狗狗，从配音开始'}</Text>
+          <Text style={styles.toolbarExtra} onPress={this._pickVideo}>更换视频</Text>
+        </View>
+
+        <View style={styles.page}>
+          {
+            this.state.previewVideo
+            ? <View></View>
+            : <TouchableOpacity style={styles.uploadContainer}
+              onPress={this._pickVideo}>
+              <View style={styles.uploadBox}>
+                <Image source={require('../assets/images/record.png')} style={styles.uploadIcon} />
+                <Text style={styles.uploadTitle}>点我上传视频</Text>
+                <Text style={styles.uploadDesc}>建议时长不超过 20 秒</Text>
+              </View>
+            </TouchableOpacity>
+          }
+        </View>
       </View>
     )
   }
@@ -19,8 +51,249 @@ var Edit = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
   },
+
+  toolbar: {
+    flexDirection: 'row',
+    paddingTop: 25,
+    paddingBottom: 12,
+    backgroundColor: '#ee735c'
+  },
+
+  toolbarTitle: {
+    flex: 1,
+    fontSize: 16,
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: '600'
+  },
+
+  toolbarExtra: {
+    position: 'absolute',
+    right: 10,
+    top: 26,
+    color: '#fff',
+    textAlign: 'right',
+    fontWeight: '600',
+    fontSize: 14
+  },
+
+  page: {
+    flex: 1,
+    alignItems: 'center'
+  },
+
+  uploadContainer: {
+    marginTop: 90,
+    width: width - 40,
+    paddingBottom: 10,
+    borderWidth: 1,
+    borderColor: '#ee735c',
+    justifyContent: 'center',
+    borderRadius: 6,
+    backgroundColor: '#fff'
+  },
+
+  uploadTitle: {
+    marginBottom: 10,
+    textAlign: 'center',
+    fontSize: 16,
+    color: '#000'
+  },
+
+  uploadDesc: {
+    color: '#999',
+    textAlign: 'center',
+    fontSize: 12
+  },
+
+  uploadIcon: {
+    width: 110,
+    resizeMode: 'contain'
+  },
+
+  uploadBox: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  videoContainer: {
+    width: width,
+    justifyContent: 'center',
+    alignItems: 'flex-start'
+  },
+
+  videoBox: {
+    width: width,
+    height: height * 0.6
+  },
+
+  video: {
+    width: width,
+    height: height * 0.6,
+    backgroundColor: '#333'
+  },
+
+  progressTipBox: {
+    width: width,
+    height: 30,
+    backgroundColor: 'rgba(244,244,244,0.65)'
+  },
+
+  progressTip: {
+    color: '#333',
+    width: width - 10,
+    padding: 5
+  },
+
+  progressBar: {
+    width: width
+  },
+
+  recordBox: {
+    width: width,
+    height: 60,
+    alignItems: 'center'
+  },
+
+  recordIconBox: {
+    width: 68,
+    height: 68,
+    marginTop: -30,
+    borderRadius: 34,
+    backgroundColor: '#ee735c',
+    borderWidth: 1,
+    borderColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  recordIcon: {
+    fontSize: 58,
+    backgroundColor: 'transparent',
+    color: '#fff'
+  },
+
+  countBtn: {
+    fontSize: 32,
+    fontWeight: '600',
+    color: '#fff'
+  },
+
+  recordOn: {
+    backgroundColor: '#ccc'
+  },
+
+  previewBox: {
+    width: 80,
+    height: 30,
+    position: 'absolute',
+    right: 10,
+    bottom: 10,
+    borderWidth: 1,
+    borderColor: '#ee735c',
+    borderRadius: 3,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  previewIcon: {
+    marginRight: 5,
+    fontSize: 20,
+    color: '#ee735c',
+    backgroundColor: 'transparent'
+  },
+
+  previewText: {
+    fontSize: 20,
+    color: '#ee735c',
+    backgroundColor: 'transparent'
+  },
+
+  uploadAudioBox: {
+    width: width,
+    height: 60,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  uploadAudioText: {
+    width: width - 20,
+    padding: 5,
+    borderWidth: 1,
+    borderColor: '#ee735c',
+    borderRadius: 5,
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#ee735c'
+  },
+
+  modalContainer: {
+    width: width,
+    height: height,
+    paddingTop: 50,
+    backgroundColor: '#fff'
+  },
+
+  closeIcon: {
+    position: 'absolute',
+    fontSize: 32,
+    right: 20,
+    top: 30,
+    color: '#ee735c'
+  },
+
+  loadingBox: {
+    width: width,
+    height: 50,
+    marginTop: 10,
+    padding: 15,
+    alignItems: 'center'
+  },
+
+  loadingText: {
+    marginBottom: 10,
+    textAlign: 'center',
+    color: '#333'
+  },
+
+  fieldBox: {
+    width: width - 40,
+    height: 36,
+    marginTop: 30,
+    marginLeft: 20,
+    marginRight: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eaeaea',
+  },
+
+  inputField: {
+    height: 36,
+    textAlign: 'center',
+    color: '#666',
+    fontSize: 14
+  },
+
+  submitBox: {
+    marginTop: 50,
+    padding: 15
+  },
+
+  btn: {
+    marginTop: 65,
+    padding: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    backgroundColor: 'transparent',
+    borderColor: '#ee735c',
+    borderWidth: 1,
+    borderRadius: 4,
+    color: '#ee735c'
+  }
 });
 
 module.exports = Edit;
